c62ac2ecc85b6425598f94e3ba64eae24d88cdd1
Nelson Chan <144390749+nchan50@users.noreply.github.com>
2025-05-29 09:39:10 -0400

    combined

--- main.py
+++ main.py
@@ -1,91 +1,5 @@
 Web VPython 3.2
 
-scene.camera.pos = vec(-100, 0, -100)
-scene.camera.axis = vec(100, 0, 100)
-scene.fov = pi/5
-scene.autoscale = False
-SCALE = 5
-
-dogs = []
-background = cone(pos=vec(0, -1.25 * scene.camera.pos.mag *  SCALE, 0), axis=vec(0, 1,0), texture="https://raw.githubusercontent.com/nchan50/BestDog/refs/heads/main/the_noble_hot_dog%20(1).png",length=2.5 * scene.camera.pos.mag * SCALE, radius=0.75 * scene.camera.pos.mag * SCALE)
-
-all_presets = []
-immutable = True
-
-def presets(evt):
-    evt.color = color.white
-    evt.background = color.black
-    for button in all_presets:
-        if button != evt and button.color == color.white:
-            button.color = color.black
-            button.background = color.white
-
-def create_dog():
-    global immutable
-    if immutable:
-        shaft = cylinder(length=15, radius = 1, axis=vec(1, 1, 1), color=color.red)
-        shaft.pos = vec(-1, -1, -1) * shaft.length / (2 * sqrt(3)) 
-        end1 = sphere(pos=vec(1, 1, 1) * shaft.length / (2 * sqrt(3)), radius=shaft.radius, color=color.red)
-        end2 = sphere(pos=vec(-1, -1, -1) * shaft.length / (2 * sqrt(3)), radius=shaft.radius, color=color.red)
-        dogs.append([end1, end2, shaft])
-    immutable = False
-    
-def adjust_dog(evt):
-    if evt.id is 'r' and not immutable:
-        for shape in dogs[-1]:
-            shape.radius = evt.value
-        rt.text = '{:1.2f}'.format(evt.value)
-    if evt.id is 'l' and not immutable:
-        for shape in dogs[-1]:
-            shape.pos *= evt.value / dogs[-1][2].length
-        dogs[-1][2].length = evt.value
-        lt.text = '{:1.2f}'.format(evt.value)
-
-def save_dog():
-    global immutable
-    immutable = True
-
-scene.title = "Presets: \n"
-all_presets.append(button(text="One Hotdog", pos=scene.title_anchor, bind=presets))
-all_presets.append(button(text="Hotdogs in Series", pos=scene.title_anchor, bind=presets))
-all_presets.append(button(text="Hotdogs in Parallel", pos=scene.title_anchor, bind=presets))
-all_presets.append(button(text="Hotdog and Resistor in Series", pos=scene.title_anchor, bind=presets))
-all_presets.append(button(text="Hotdog and Resistor in Parallel", pos=scene.title_anchor, bind=presets))
-scene.append_to_title('\n')
-all_presets.append(button(text="Hotdog and Capacitor in Series", pos=scene.title_anchor, bind=presets))
-all_presets.append(button(text="Hotdog and Capacitor in Parallel", pos=scene.title_anchor, bind=presets))
-all_presets.append(button(text="Hotdog and Inductor in Series", pos=scene.title_anchor, bind=presets))
-all_presets.append(button(text="Hotdog and Inductor in Parallel", pos=scene.title_anchor, bind=presets))
-
-scene.caption = 'Create Your Hot Dog: '
-button(bind = create_dog, text= 'Create me!')
-scene.append_to_caption('\n')
-rs = slider(bind = adjust_dog, max = 2, min = 0.25, step = 0.1, value = 1, id = 'r')
-scene.append_to_caption('Radius: ')
-rt = wtext(text='{:1.2f}'.format(rs.value))
-scene.append_to_caption(' cm\n')
-ls = slider(bind = adjust_dog, max = 30, min = 5, step = 0.1, value = 15, id = 'l')
-scene.append_to_caption('Length: ')
-lt = wtext(text='{:1.2f}'.format(ls.value))
-scene.append_to_caption(' cm\n')
-button(bind = save_dog, text= 'Save me!')
-    
-while True:
-    rate(10)
-    a = background.pos.z / sqrt(background.pos.x ** 2 + background.pos.y ** 2)
-    camera_pos = scene.camera.pos
-    cone_closest = vec(camera_pos.x/(2 + a), (camera_pos.y - background.pos.x)/ (2 + a), 0)
-    cone_closest.z = a * sqrt(cone_closest.x ** 2, cone_closest.y ** 2) + background.pos.x
-    if sqrt((cone_closest.x - camera_pos.x) ** 2 + (cone_closest.y - camera_pos.y) ** 2 + (cone_closest.z - camera_pos.z) ** 2) < background.radius / 9:
-        background.pos = vec(0, -1.5 * scene.camera.pos.mag *  SCALE, 0)
-        background.length = 2.5 * scene.camera.pos.mag * SCALE
-        background.radius = 1.75 * scene.camera.pos.mag * SCALE
-
-
-
-
-
-
 ## Circuit simulation
 
 #ordered list for both parallel and series
@@ -93,6 +7,64 @@ while True:
 
 ##################
 # CONSTANTS
+
+SCALE = 5
+scene.camera.pos = vec(-30, 0, -30)
+scene.camera.axis = vec(30, 0, 30)
+scene.fov = pi/8
+scene.autoscale = False
+
+dogs = []
+background = cone(pos=vec(0, -1.25 * scene.camera.pos.mag *  SCALE, 0), axis=vec(0, 1,0), texture="https://raw.githubusercontent.com/nchan50/BestDog/refs/heads/main/the_noble_hot_dog%20(1).png",length=2.5 * scene.camera.pos.mag * SCALE, radius=0.75 * scene.camera.pos.mag * SCALE)
+
+battery = box(pos = vec(15, 0, -15), axis = vec(0, 1, 0), length = 10, height = 10, width = 6) 
+change_presets = []
+all_presets = {
+     'One Hotdog' :
+     [cylinder(pos = vec(-15, 0, 15) - vec(0, 1, 0) * 15/2, length=15, radius = 1, axis=vec(0, 1, 0), color=color.red, visible = False),
+     sphere(pos= vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2, radius=1, color=color.red, visible = False),
+     sphere(pos= vec(-15, 0, 15) -  vec(0, 1, 0) * 15 / 2, radius=1, color=color.red, visible = False),
+     curve(pos = [vec(15, 0, -15) + vec(0, 5, 0), vec(15, 0, -15) + vec(0, 20, 0), vec(-15, 0, 15) + vec(0, 20, 0), vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(15, 0, -15) + vec(0, 5, 0), vec(15, 0, -15) - vec(0, 20, 0), vec(-15, 0, 15) - vec(0, 20, 0), vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2], color = color.yellow, radius = 0.3, visible = False)],
+     'Hotdog and Resistor in Series':
+     [curve(pos = [vec(-6, 20, 6), vec(-6, 20, 6) + vec(0, 3, 0) + vec(1, 0, -1) * 1, vec(-6, 20, 6) - vec(0, 3, 0) + vec(1, 0, -1) * 2, vec(-6, 20, 6) + vec(0, 3, 0) + vec(1, 0, -1) * 3, vec(-6, 20, 6) - vec(0, 3, 0) + vec(1, 0, -1) * 4, vec(-6, 20, 6) + vec(0, 3, 0) + vec(1, 0, -1) * 5, vec(-6, 20, 6) - vec(0, 3, 0) + vec(1, 0, -1) * 6, vec(-6, 20, 6) + vec(0, 3, 0) + vec(1, 0, -1) * 7, vec(-6, 20, 6) - vec(0, 3, 0) + vec(1, 0, -1) * 8, vec(-6, 20, 6) + vec(1, 0, -1) * 9], radius = 0.6, visible = False),
+     cylinder(pos = vec(-15, 0, 15) - vec(0, 1, 0) * 15/2, length=15, radius = 1, axis=vec(0, 1, 0), color=color.red, visible = False),
+     sphere(pos= vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2, radius=1, color=color.red, visible = False),
+     sphere(pos= vec(-15, 0, 15) -  vec(0, 1, 0) * 15 / 2, radius=1, color=color.red, visible = False),
+     curve(pos = [vec(15, 0, -15) + vec(0, 5, 0), vec(15, 0, -15) + vec(0, 20, 0), vec(-6, 20, 6) + vec(1, 0, -1) * 9], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(-6, 20, 6), vec(-15, 0, 15) + vec(0, 20, 0), vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(15, 0, -15) + vec(0, 5, 0), vec(15, 0, -15) - vec(0, 20, 0), vec(-15, 0, 15) - vec(0, 20, 0), vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2], color = color.yellow, radius = 0.3, visible = False)],
+     'Hotdog and Capacitor in Parallel':
+     [box(pos = vec(1, 2, -1), axis = vec(0, 1, 0), length = 1, height = 10, width = 10, color = color.red, visible = False),
+     box(pos = vec(1, -2, -1), axis = vec(0, 1, 0), length = 1, height = 10, width = 10, color = color.blue, visible = False), 
+     cylinder(pos = vec(-15, 0, 15) - vec(0, 1, 0) * 15/2, length=15, radius = 1, axis=vec(0, 1, 0), color=color.red, visible = False),
+     sphere(pos= vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2, radius=1, color=color.red, visible = False),
+     sphere(pos= vec(-15, 0, 15) -  vec(0, 1, 0) * 15 / 2, radius=1, color=color.red, visible = False),
+     curve(pos = [vec(15, 0, -15) + vec(0, 5, 0), vec(15, 0, -15) + vec(0, 20, 0), vec(-15, 0, 15) + vec(0, 20, 0), vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(15, 0, -15) + vec(0, 5, 0), vec(15, 0, -15) - vec(0, 20, 0), vec(-15, 0, 15) - vec(0, 20, 0), vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(1, 2, -1), vec(1, 20, -1)], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(1, -2, -1), vec(1, -20, -1)], color = color.yellow, radius = 0.3, visible = False)],
+     'Hotdog and Inductor in Series': 
+     [helix(pos = vec(-6, 20, 6), axis = vec(1, 0, -1), length = 13, coils = 12, radius = 4, thickness = 0.8, color = color.black, visible = False),
+     cylinder(pos = vec(-15, 0, 15) - vec(0, 1, 0) * 15/2, length=15, radius = 1, axis=vec(0, 1, 0), color=color.red, visible = False),
+     sphere(pos= vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2, radius=1, color=color.red, visible = False),
+     sphere(pos= vec(-15, 0, 15) -  vec(0, 1, 0) * 15 / 2, radius=1, color=color.red, visible = False),
+     curve(pos = [vec(-6, 20, 6) + vec(1, 0, -1) * 9, vec(-6, 20, 6) + vec(1, 0, -1) * 9 + vec(1, 0, 1) * 3], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(-6, 20, 6), vec(-15, 0, 15) + vec(0, 20, 0), vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(-6, 20, 6), vec(-15, 0, 15) + vec(0, 20, 0), vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(15, 0, -15) + vec(0, 5, 0), vec(15, 0, -15) + vec(0, 20, 0), vec(-6, 20, 6) + vec(1, 0, -1) * 9], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(-6, 20, 6) + vec(1, 0, 1) * 3, vec(-6, 20, 6)], color = color.yellow, radius = 0.3, visible = False),
+     curve(pos = [vec(15, 0, -15) + vec(0, 5, 0), vec(15, 0, -15) - vec(0, 20, 0), vec(-15, 0, 15) - vec(0, 20, 0), vec(-15, 0, 15) +  vec(0, 1, 0) * 15/ 2], color = color.yellow, radius = 0.3, visible = False)]
+     }
+     
+current_preset = []
+     
+temp = graph(title='Hotdog 1', xtitle='Time(s)', ytitle='Energy Dissipated(J)', xmin=0, ymin=0, xmax = 120, ymax = 500)
+a = gcurve(graph=temp)
+for x in range(0, 120):
+    a.plot(x, 150 - 50 ^ x)
+
+immutable = True
 
 HOTDOG_RESISTIVITY = 346 * 100 #Ohm/cm
 
@@ -160,7 +132,7 @@ def calc_resistance(resistivity,radius,length):
     return resistivity * circleArea(radius) / length
 
 def create_dog(radius, length): #radius and length in METERS
-    hotdog = CIRCEL("resistor",HOTDOG_RESISTIVITY*pi*(radius**2)
+    hotdog = CIRCEL("resistor",HOTDOG_RESISTIVITY*pi*(radius**2))
     return hotdog
 
 
@@ -178,11 +150,87 @@ def batt_dog(voltage):
 
 #def preset_circuits()
 
+def presets(evt):
+    global change_presets
+    global current_preset
+    global all_presets
+    evt.color = color.white
+    evt.background = color.black
+    for button in change_presets:
+        if button != evt and button.color == color.white:
+            button.color = color.black
+            button.background = color.white
+    for item in current_preset:
+        item.visible = False
+    current_preset = all_presets[evt.text]
+    for item in current_preset:
+        item.visible = True
 
+def create_dog():
+    global immutable
+    if immutable:
+        shaft = cylinder(length=15, radius = 1, axis=vec(1, 1, 1), color=color.red)
+        shaft.pos = vec(-1, -1, -1) * shaft.length / (2 * sqrt(3)) 
+        end1 = sphere(pos=vec(1, 1, 1) * shaft.length / (2 * sqrt(3)), radius=shaft.radius, color=color.red)
+        end2 = sphere(pos=vec(-1, -1, -1) * shaft.length / (2 * sqrt(3)), radius=shaft.radius, color=color.red)
+        dogs.append([end1, end2, shaft])
+    immutable = False
+    
+def adjust_dog(evt):
+    if evt.id is 'r' and not immutable:
+        for shape in dogs[-1]:
+            shape.radius = evt.value
+        rt.text = '{:1.2f}'.format(evt.value)
+    if evt.id is 'l' and not immutable:
+        for shape in dogs[-1]:
+            shape.pos *= evt.value / dogs[-1][2].length
+        dogs[-1][2].length = evt.value
+        lt.text = '{:1.2f}'.format(evt.value)
 
+def save_dog():
+    global immutable
+    immutable = True
 
+def temp():
+    return
 
+scene.title = "Presets: \n"
+change_presets.append(button(text="One Hotdog", pos=scene.title_anchor, bind=presets))
+change_presets.append(button(text="Hotdogs in Series", pos=scene.title_anchor, bind=presets))
+change_presets.append(button(text="Hotdogs in Parallel", pos=scene.title_anchor, bind=presets))
+change_presets.append(button(text="Hotdog and Resistor in Series", pos=scene.title_anchor, bind=presets))
+change_presets.append(button(text="Hotdog and Resistor in Parallel", pos=scene.title_anchor, bind=presets))
+scene.append_to_title('\n')
+change_presets.append(button(text="Hotdog and Capacitor in Series", pos=scene.title_anchor, bind=presets))
+change_presets.append(button(text="Hotdog and Capacitor in Parallel", pos=scene.title_anchor, bind=presets))
+change_presets.append(button(text="Hotdog and Inductor in Series", pos=scene.title_anchor, bind=presets))
+change_presets.append(button(text="Hotdog and Inductor in Parallel", pos=scene.title_anchor, bind=presets))
 
-
-
-
+scene.caption = 'User Options: \n'
+scene.append_to_caption('Start Simulation: ')
+button(bind = temp, text= 'Turn on Battery')
+scene.append_to_caption('\nMake Your Own Hot Dog: ')
+button(bind = create_dog, text= 'Create!')
+button(bind = save_dog, text= 'Save!')
+button(bind = temp, text= 'Attatch to Circuit!')
+scene.append_to_caption('\n Adjust Your Hotdogs \n')
+rs = slider(bind = adjust_dog, max = 2, min = 0.25, step = 0.1, value = 1, id = 'r')
+scene.append_to_caption('Radius: ')
+rt = wtext(text='{:1.2f}'.format(rs.value))
+scene.append_to_caption(' cm\n')
+ls = slider(bind = adjust_dog, max = 30, min = 5, step = 0.1, value = 15, id = 'l')
+scene.append_to_caption('Length: ')
+lt = wtext(text='{:1.2f}'.format(ls.value))
+scene.append_to_caption(' cm\n')
+ev = winput(bind=temp, prompt='Adjust Circuit Element Values:', type='numeric')
+    
+while True:
+    rate(10)
+    a = background.pos.z / sqrt(background.pos.x ** 2 + background.pos.y ** 2)
+    camera_pos = scene.camera.pos
+    cone_closest = vec(camera_pos.x/(2 + a), (camera_pos.y - background.pos.x)/ (2 + a), 0)
+    cone_closest.z = a * sqrt(cone_closest.x ** 2, cone_closest.y ** 2) + background.pos.x
+    if sqrt((cone_closest.x - camera_pos.x) ** 2 + (cone_closest.y - camera_pos.y) ** 2 + (cone_closest.z - camera_pos.z) ** 2) < background.radius / 9:
+        background.pos = vec(0, -1.5 * scene.camera.pos.mag *  SCALE, 0)
+        background.length = 2.5 * scene.camera.pos.mag * SCALE
+        background.radius = 1.75 * scene.camera.pos.mag * SCALE
